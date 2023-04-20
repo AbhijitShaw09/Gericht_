2742ee878c92b07c39b6f5f9d0a39eec29df65ce
AbhijitShaw09 <78562439+AbhijitShaw09@users.noreply.github.com>
2023-04-20 15:21:10 +0530

    Added initial files of Gericht

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import './Chef.css';
+
+const Chef = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+      
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Lorem, ipsum dolor sit amet consectetur adipisicing elit!</p>
+        </div>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet adipisicing. Quos perspiciatis  amet repellendus veritatis numquam debitis perferendis labore? Expedita tenetur maxime vel assumenda.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p className='p__cormorant'>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
